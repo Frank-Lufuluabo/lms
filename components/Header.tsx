f29f9f1759369f72ad58d2dcbcf7f98c0b64e039
frankkalengay@gmail.com <frankkalengay@gmail.com>
2025-02-28 22:09:16 +0200

    Implementing Light & Dark mode with Shadcn

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import DarkModeToggle from './DarkModeToggle'
+
+function Header() {
+  return (
+    <div>
+      <DarkModeToggle />
+    </div>
+  )
+}
+
+export default Header
